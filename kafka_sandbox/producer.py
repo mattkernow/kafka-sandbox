ba18601087b086f21cc1a5cb5b12f2c0c54750ce
Walsh, Matt UK-EDI <mwalsh@illuminate.solutions>
2020-06-23 13:36:35 +0100

    send many messages

--- kafka_sandbox/producer.py
+++ kafka_sandbox/producer.py
@@ -16,25 +16,30 @@ def serializer(value):
         raise KafkaDemoProducerError('Message must be dict type')
 
 
-async def send_one():
+async def send_one(num):
     producer = AIOKafkaProducer(
         loop=loop, 
         bootstrap_servers='localhost:9092',
         # Wait only the broker leader to succeed
-        acks=0,
+        acks=1,
         value_serializer=serializer,
         compression_type="gzip",)
     # Get cluster layout and initial topic/partition leadership information
     await producer.start()
     try:
         # Produce message
-        response = await producer.send_and_wait("first_topic", {"ping": "pong"})
+        response = await producer.send_and_wait("first_topic", {"ping": "pong", "number": num})
         print(f'\nRecieved new metadata...\nTopic: {response.topic}\nPartition: {response.partition}\nOffset: {response.offset}\nTimestamps: {response.timestamp}')
     finally:
         # Wait for all pending messages to be delivered or expire.
         await producer.stop()
 
-def main():
+async def send_many():
     print('Starting...')
-    loop.run_until_complete(send_one())
+    all_msgs = [send_one(x) for x in range(100)]
+    gather_all_msgs = await asyncio.gather(*all_msgs)
     print('Finishing...')
+
+
+def main():
+    loop.run_until_complete(send_many())
